7060367992d035fff0eaa337bda534d3cd03fd14
Jakob <45669207+gue-ni@users.noreply.github.com>
2024-11-01 10:16:00 +0100

    Update geometry.cpp

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -24,6 +24,35 @@ bool ray_vs_sphere(const Ray& r, const Sphere& s, float& t)
   return true;
 }
 
+bool hit(const Ray& r const Sphere& s) {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
+
+        auto discriminant = h*h - a*c;
+        if (discriminant < 0)
+            return false;
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        auto root = (h - sqrtd) / a;
+        if (!ray_t.surrounds(root)) {
+            root = (h + sqrtd) / a;
+            if (!ray_t.surrounds(root))
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
+
+        return true;
+}
+
 bool ray_vs_triangle(const Ray&, const Triangle&, float& t) { return false; }
 
 std::optional<Intersection> Primitive::intersect(const Ray& ray) const
