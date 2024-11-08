553cfa6501fd8b5375b5067fc435a60b83ec7bf4
Jakob <jakob.g.maier@gmail.com>
2024-11-08 10:47:06 +0100

    Remove test_scene_1

--- src/main.cpp
+++ src/main.cpp
@@ -76,63 +76,6 @@ static void from_json(const json& j, Config& c)
   c.spheres = j["spheres"].get<std::vector<SimpleSphere>>();
 }
 
-std::tuple<std::unique_ptr<Scene>, std::unique_ptr<Camera>> test_scene_1()
-{
-  auto scene = std::make_unique<Scene>();
-
-#if _WIN32
-  std::filesystem::path models = "C:/Users/jakob/Documents/Projects/pathtracer/doc/models";
-#else
-  std::filesystem::path models = "/home/pi/pathtracer/doc/models";
-#endif
-
-  auto white = scene->add_material(Material(glm::dvec3(1.0)));
-  auto red = scene->add_material(Material(glm::dvec3(.77, 0, 0)));
-  auto green = scene->add_material(Material(glm::dvec3(0, 1, 0)));
-  auto blue = scene->add_material(Material(glm::dvec3(0, 0, .77)));
-  auto emissive = scene->add_material(Material(glm::dvec3(1), glm::dvec3(1) * 10.0));
-  auto glass = scene->add_material(Material(Material::TRANSMISSIVE, glm::dvec3(1), glm::dvec3(0)));
-  auto pink = scene->add_material(Material(glm::dvec3(1), rgb(252, 15, 192) * 5.0));
-  auto mirror = scene->add_material(Material(Material::SPECULAR, glm::dvec3(1), glm::dvec3(0)));
-
-  auto tex = scene->add_material(Material());
-  tex->albedo = glm::dvec3(1, 0, 0);
-  tex->texture = new Image();
-  tex->texture->load(models / std::filesystem::path("uv-test.png"));
-
-#if 1
-  auto mesh = scene->load_obj(models / std::filesystem::path("cornell_box.obj"));
-
-  AABB bbox = compute_bounding_volume(mesh.begin(), mesh.end());
-  std::cout << "Mesh Size: " << bbox.size() << ", Mesh Center: " << bbox.center() << std::endl;
-
-  scene->add_primitives(mesh.begin(), mesh.end());
-#endif
-#if 0
-  scene->add_primitive(Primitive(Sphere(glm::dvec3(0.0, -1e6, 0.0), 1e6), white));
-#endif
-#if 0
-  // light
-  scene->add_primitive(Primitive(Sphere(glm::dvec3(0, 170, 0), 30), emissive));
-#endif
-#if 1
-  scene->add_primitive(Primitive(Sphere(glm::dvec3(+70, 20, 0), 20), mirror));
-#endif
-#if 1
-  scene->add_primitive(Primitive(Sphere(glm::dvec3(-70, 20, 0), 20), tex));
-#endif
-
-  std::unique_ptr<Camera> camera = std::make_unique<Camera>(640, 360);
-  camera->set_position(glm::dvec3(0, 50, 230));
-
-  std::cout << "Camera Position: " << camera->position() << std::endl;
-  std::cout << "Camera Direction: " << camera->direction() << std::endl;
-
-  scene->compute_bvh();
-
-  return std::make_tuple(std::move(scene), std::move(camera));
-}
-
 std::tuple<std::unique_ptr<Scene>, std::unique_ptr<Camera>> setup_scene(const Config& config)
 {
   auto scene = std::make_unique<Scene>();
@@ -172,11 +115,12 @@ std::tuple<std::unique_ptr<Scene>, std::unique_ptr<Camera>> setup_scene(const Co
 int main(int argc, char** argv)
 {
   std::filesystem::path config_path;
-  std::filesystem::path result_path;
 
-  if (2 <= argc) {
-    config_path = std::filesystem::path(argv[1]);
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <path to config.json>" << std::endl;
   }
+
+  config_path = std::filesystem::path(argv[1]);
 
   std::ifstream file(config_path);
 
